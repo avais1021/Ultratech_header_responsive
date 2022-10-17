62a5ecc8106fa12fb9821d257d8eadb61bf895bd
Owes Ansari <Owes.Ansari@teknopoint.local>
2022-10-17 12:31:09 +0530

    changes

--- script.js
+++ script.js
@@ -44,6 +44,70 @@ function removeOpen (index1) {
 
 }
 
+// --------------------------get in touch-----------------------------------------------------------
+
+const getinTouch = document.querySelector(".getintouch");
+const popupSec = document.querySelector(".popup_sec");
+    getinTouch.addEventListener("click" , ()=> {
+        popupSec.style.display = "block";
+        document.body.setAttribute("style","background: rgb(0 0 0 / 59%);");
+    });
+
+const closeBtn = document.querySelector(".close_btn");
+    closeBtn.addEventListener("click" , ()=> {
+        popupSec.style.display = "none";
+        document.body.setAttribute("style","background: unset;");
+    });
+
+
+
+    // --------------------------------form validation----------------------- 
+
+    function validate () {
+        console.log(student_name.name.value);
+
+        var name = document.student_name.name;
+        var mobileno = document.student_name.mobile_number;
+        var pincode = document.student_name.pincode;
+        var gender = document.student_name.gender;
+        var category = document.student_name.your_category;
+        var checkbox = document.student_name.checkbox;
+        if(name.value.length <= 0){
+            alert("Nmae is required");
+            name.focus();
+            return false;
+        }
+        if(mobileno.value.length <= 0){
+            alert("Mobile no is required");
+            mobileno.focus();
+            return false;
+        }
+        if(pincode.value.length <= 0){
+            alert("Pincode is required");
+            pincode.focus();
+            return false;
+        }
+        if(gender.value.length <= 0){
+            alert("Gender is Required");
+            gender.focus();
+            return false;
+        }
+        if (checkbox.value == false){
+            alert("checkbox is required");
+            checkbox.focus();
+            return false;
+        }
+        console.log(checkbox.length);
+        if(category.value == "Select category" ){
+            alert("category is required");
+            category.focus();
+            return false;
+        }
+     
+
+        return false;
+    }
+
 
 
 
